f46f4af3d8cbb9d5989112e7ec67c670e3787875
Hurlyburly2 <dougdougmann@gmail.com>
2019-05-25 21:52:09 -0400

    add more than one css file in styleurls

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mw-app',
   templateUrl: 'app/app.component.html',
-  styleUrls: [`app/app.component.css`]
+  styleUrls: [`app/app.component.css`, `app/app.component_again.css`]
 })
 export class AppComponent {}
